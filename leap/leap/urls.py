eda7f25a367709dc538306c3a34df80bbe270ae4
conorshirren <cshirren@gmail.com>
2020-12-03 20:59:31 +0000

    [wip] Implemented Rest Framework and Swagger UI
    # Added Requirements.txt

--- leap/leap/urls.py
+++ leap/leap/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework_simplejwt import views as jwt_views
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Leap API",
+      default_version='v1',
+      description="Leap API Documentation",
+      terms_of_service="https://www.leap.ie/policies/terms/",
+      contact=openapi.Contact(email="cshirren@gmail.com"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
