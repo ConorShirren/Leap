7329dd255964ca6768f66b5753e63cfc80374292
conorshirren <cshirren@gmail.com>
2020-12-07 21:32:42 +0000

    Angular 11 Upgrade

--- client/src/app/core/services/dashboard.service.ts
+++ client/src/app/core/services/dashboard.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
