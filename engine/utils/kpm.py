6174aa605dd48ce2c47e30f8c39c7254f5070e2c
conorshirren <cshirren@gmail.com>
2020-12-08 22:26:47 +0000

    [wip] updated key performance metrics

--- engine/utils/kpm.py
+++ engine/utils/kpm.py
@@ -17,4 +17,18 @@ class KeyPerformanceMetrics:
     def getKPM(self):
         stravaApi = _api()
         data = stravaApi.GetActivities()
-        return data
+        latestRun =  data[data['type'] == "Run"].iloc[0]
+        pr5k = data[(data['distance'] >= 5000) & (data['distance'] <= 5050) & (data['type'] == "Run")].sort_values(by=['elapsed_time']).iloc[0]
+        pr10k = data[(data['distance'] >= 10000) & (data['distance'] <= 10050) & (data['type'] == "Run")].sort_values(by=['elapsed_time']).iloc[0]
+        pr2k = data[(data['distance'] >= 2000) & (data['distance'] <= 2050) & (data['type'] == "Run")].sort_values(by=['elapsed_time']).iloc[0]
+        pr8k = data[(data['distance'] >= 8000) & (data['distance'] <= 8050) & (data['type'] == "Run")].sort_values(by=['elapsed_time']).iloc[0]
+
+        # Breakdown of each exercise type for pie chart
+
+        return {
+            "latest_run":latestRun,
+            "pr5k":pr5k,
+            "pr10k":pr10k,
+            "pr2k":pr2k,
+            "pr8k":pr8k
+        }
